c4b176b340fd7b60672702ee33d9f831b7a676ec
Kirill Gavrilov <gavrilov-kirill_@mail.ru>
2020-04-08 15:29:28 +0300

    fixed button design

--- Calculator/Model/SettingButton.swift
+++ Calculator/Model/SettingButton.swift
@@ -10,7 +10,7 @@ import UIKit
 
 @IBDesignable
 class SettingButton: UIButton {
-
+    
     @IBInspectable var roundButton: Bool = false {
         didSet {
             if roundButton {
@@ -18,11 +18,19 @@ class SettingButton: UIButton {
             }
         }
     }
-    
-    override func prepareForInterfaceBuilder() {
-        if roundButton {
-            layer.cornerRadius = frame.height / 2
-        }
+    @IBInspectable var borderWidth: CGFloat {
+        set { layer.borderWidth = newValue }
+        get { return layer.borderWidth }
     }
-
+        @IBInspectable var borderColor: UIColor? {
+        set { layer.borderColor = newValue?.cgColor  }
+        get { return layer.borderColor as? UIColor}
+        }
+        
+        override func prepareForInterfaceBuilder() {
+        if roundButton {
+        layer.cornerRadius = frame.height / 2
+        }
+        }
+        
 }
